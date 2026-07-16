1fb55ab75b85c0e44f0d8b81be16e1b05215989e
Well-AlvesDev <weslleykholson@gmail.com>
2026-07-16 15:57:06 -0300

    Refactor code structure for improved readability and maintainability

--- agendamento/js/components/ServiceGrid.js
+++ agendamento/js/components/ServiceGrid.js
@@ -1,21 +1,23 @@
 window.ServiceGrid = {
-    props: ['services', 'selectedId', 'locked'],
-    emits: ['select'],
-    template: `
+  props: ['services', 'selectedIds', 'locked'],
+  emits: ['toggle'],
+  template: `
     <div class="service-grid" :class="{ locked }">
       <div
         v-for="(s, i) in services"
         :key="s.id"
         class="service-card"
-        :class="{ selected: selectedId === s.id, disabled: locked && selectedId !== s.id }"
+        :class="{ selected: selectedIds.includes(s.id), disabled: locked && !selectedIds.includes(s.id) }"
         :style="{ animationDelay: (i * 0.06) + 's' }"
-        @click="!locked && $emit('select', s)"
+        @click="!locked && $emit('toggle', s)"
       >
-        <div class="s-icon"><i :class="s.icon"></i></div>
-        <div class="s-name">{{ s.name }}</div>
-        <div class="s-meta">
-          <span>{{ s.duration }} min</span>
-          <span class="s-price">R$ {{ s.price }}</span>
+        <div class="service-image" :style="{ backgroundImage: 'url(' + s.image + ')' }"></div>
+        <div class="service-details">
+          <div class="s-name">{{ s.name }}</div>
+          <div class="s-meta">
+            <span class="s-duration">{{ s.duration }} min</span>
+            <span class="s-price">R$ {{ s.price }}</span>
+          </div>
         </div>
       </div>
     </div>
